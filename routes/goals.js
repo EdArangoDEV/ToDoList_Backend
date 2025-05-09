0dfd216b2ddc7c38d85c40d5eff6076d63cd30a5
Randol Gutierrez <randolg@GBODEGAS.INTERNO>
2025-05-09 15:24:04 -0600

    Agregando cambios con routes de tasks y goals 09052025

---
+++ routes/goals.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+let goals = [
+  { id: 1, name: "Goal 1", description: "Description for Goal 1" },
+  { id: 2, name: "Goal 2", description: "Description for Goal 2" },
+];
+
+router.get("/getGoals", function (req, res, next) {
+  res.json(goals);
+});
+
+router.delete("/deleteGoal/:id", function (req, res, next) {
+  const goalId = parseInt(req.params.id);
+  goals = goals.filter((goal) => goal.id !== goalId);
+  res.json({ message: "Goal deleted successfully" });
+});
+
+router.post("/addGoal", function (req, res, next) {
+  // Verificamos que goals exista y tenga elementos con id numérico válido
+  const ids = goals
+    .map((goal) => Number(goal.id)) // Convertimos a número por si acaso
+    .filter((id) => !isNaN(id) && id > 0); // Filtramos ids válidos positivos
+
+  const lastId = ids.length > 0 ? Math.max(...ids) : 0;
+
+  const newGoal = {
+    id: lastId + 1,
+    name: req.body.name,
+    description: req.body.description,
+  };
+  goals.push(newGoal);
+  res.json(newGoal);
+});
+
+
+module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,36 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.json({ message: "Hello from tasks" });
+let tasks = [
+  { id: 1, name: "Task 1", description: "Description for Task 1" },
+  { id: 2, name: "Task 2", description: "Description for Task 2" },
+];
+
+router.get("/getTasks", function (req, res, next) {
+  res.json(tasks);
+});
+
+router.delete("/deleteTask/:id", function (req, res, next) {
+  const taskId = parseInt(req.params.id);
+  tasks = tasks.filter((task) => task.id !== taskId);
+  res.json({ message: "Task deleted successfully" });s
+});
+
+router.post("/addTask", function (req, res, next) {
+  // Verificamos que tasks exista y tenga elementos con id numérico válido
+  const ids = tasks
+    .map((task) => Number(task.id)) // Convertimos a número por si acaso
+    .filter((id) => !isNaN(id) && id > 0); // Filtramos ids válidos positivos
+
+  const lastId = ids.length > 0 ? Math.max(...ids) : 0;
+
+  const newTask = {
+    id: lastId + 1,
+    name: req.body.name,
+    description: req.body.description,
+  };
+  tasks.push(newTask);
+  res.json(newTask);
 });
 
 
